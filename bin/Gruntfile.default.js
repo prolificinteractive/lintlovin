4141e7a1a266498e70930d7c2d45b26fe2b7caba
prolific_conor <conor@prolificinteractive.com>
2015-02-27 18:07:03 -0500

    added prolific customizations and customhusky fork

--- bin/Gruntfile.default.js
+++ bin/Gruntfile.default.js
@@ -4,5 +4,9 @@
 var lintlovin = require('lintlovin');
 
 module.exports = function (grunt) {
-  lintlovin.initConfig(grunt);
+  lintlovin.initConfig(grunt, {}, {
+    // Keep getting "Fatal error: Dependencies not listed in package.json: MD5"
+    // had to diable
+    noDependencyCheck: true
+  });
 };
